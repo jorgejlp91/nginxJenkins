0d97f01f48f136da9bd44dff2c3c10cb2ddf6660
Jorge Lucas <jorgejlp91@gmail.com>
2017-06-12 21:41:10 -0300

    Aumentado o tempo para o efetuar o wget no jenkins

--- assets/start_nginx_jenkins.sh
+++ assets/start_nginx_jenkins.sh
@@ -1,7 +1,7 @@
 service nginx start
 service jenkins start
-echo "esperando 5 segundos ate o jenkins subir..."
-sleep 5
+echo "esperando 10 segundos ate o jenkins subir..."
+sleep 10
 wget http://localhost:8080/login?from=%2F
 cat /var/lib/jenkins/secrets/initialAdminPassword
 echo "Use essa chave para desbloquear o jenkins"
